3946826444d8b81dc55d901dde69319e42c362ba
manjeet <manjeet.chauhan@trigyn.com>
2023-07-18 19:16:33 +0530

    image changes

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("../../../public/public/planet/scene.gltf");
+  const earth = useGLTF("./planet/scene.gltf");
+  const earth1 = useGLTF("");
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
